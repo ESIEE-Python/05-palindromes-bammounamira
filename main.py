695f53268c09918f56ff157b94fe830783cf2531
bammounamira <amira.bammoun@edu.esiee.fr>
2024-09-21 11:10:45 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-#### Fonction secondaire
-
-
-def ispalindrome(p):
-
-    # votre code ici
+"""
+Ce module teste si une chaîne de caractères est un palindrome.
+"""
+def ispalindrome(s):
+    """
+    Cette fonction teste si une chaîne de caractères est un palindrome.
     
-    return False
-
-#### Fonction principale
-
+    :param s: La chaîne de caractères
+    :return: True si la chaîne est un palindrome, sinon False
+    """
+    translation_table = str.maketrans("éèêëêçùàôÂÊÎÔÛÄËÏÖÜÀÉÈÙ", "eeeeecuaoAEIOUAEIOUAEEU")
+    # Nettoyer la chaîne : enlever les espaces et mettre tout en minuscules
+    ponctuation="()[]{},?!;'.:-"
+    ponctuation_table=str.maketrans('','',ponctuation)
+    s = s.translate(ponctuation_table)
+    s = s.translate(translation_table)
+    s = ''.join(s.split()).lower()
+    return s == s[::-1]  # Retourne True si s est un palindrome, sinon False
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """
+    Fonction principale pour tester plusieurs chaînes de caractères
+    afin de déterminer si elles sont des palindromes.
+    """
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
-        print(s, ispalindrome(s))
-
-
+        print(s,ispalindrome(s))
 if __name__ == "__main__":
     main()
